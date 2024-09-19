6053a9f84c8666cc993aac28de97590b24642ad8
alhandilshad <dilshadalhan@gmail.com>
2024-09-19 19:25:39 +0500

    smit card complete

---
+++ app/cardData.js
@@ -0,0 +1,64 @@
+const cardData = [
+    {
+        imageURL: 'https://media.licdn.com/dms/image/v2/D4E03AQEfwXTMLTbUtA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1702997699915?e=1732147200&v=beta&t=7TyR2a4wUY61EHvuK6430jKUlfpq1AKILnL_3drBfhU',
+        name: 'Alhan Dilshad',
+        courseName: 'Web Development',
+        rollNo: 'WD - 177208'
+    },
+    {
+        imageURL: 'https://avatars.githubusercontent.com/u/54757723?v=4',
+        name: 'Ahmed Ashraf',
+        courseName: 'Python Programming',
+        rollNo: 'PY - 208526'
+    },
+    {
+        imageURL: 'https://media.licdn.com/dms/image/v2/D4E35AQGw6-bAhmJtlQ/profile-framedphoto-shrink_200_200/profile-framedphoto-shrink_200_200/0/1721124938039?e=1727359200&v=beta&t=l3om80_9tre7Bxj3MFtoEAWpuKAVtNga5a-134idmeM',
+        name: 'Farhan',
+        courseName: 'React Native Development',
+        rollNo: 'RND - 256700'
+    },
+    {
+        imageURL: 'https://media.licdn.com/dms/image/v2/D4D03AQEeTtt87Ft68Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1725886037934?e=1732147200&v=beta&t=TbcGcACA7KPwmDxuGXrZcgzxWtr7w9NsTXn9qVZr-a0',
+        name: 'Aarij',
+        courseName: 'Web Development',
+        rollNo: 'WD - 156248'
+    },
+    {
+        imageURL: 'https://media.licdn.com/dms/image/v2/D4D03AQFRJfHcmCtBUA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1721313670902?e=1732147200&v=beta&t=itlKOmSyaDhN1O2NBOlBDmOJgqkMW3roDL900EHzpoQ',
+        name: 'Zaki',
+        courseName: 'Flutter Development',
+        rollNo: 'FD - 190654'
+    },
+    {
+        imageURL: 'https://media.licdn.com/dms/image/v2/D4D35AQEjS0ZNpCOGSg/profile-framedphoto-shrink_200_200/profile-framedphoto-shrink_200_200/0/1721461709292?e=1727359200&v=beta&t=V6SNpznz-xTaHPCDwPnxEFi-gTus8zPwr7pydkk4LvM',
+        name: 'Ammad',
+        courseName: 'SQA Engineer',
+        rollNo: 'SA - 234567'
+    },
+    {
+        imageURL: 'https://avatars.githubusercontent.com/u/154331886?v=4',
+        name: 'Rayyan',
+        courseName: 'Software Engineer',
+        rollNo: 'SE - 178907'
+    },
+    {
+        imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEwuq64VzUpE5NIqGSUXnJT4vRWodiUB0XUA&s',
+        name: 'Aadil',
+        courseName: 'Mobile App Development',
+        rollNo: 'MAD - 289087'
+    },
+    {
+        imageURL: 'https://media.licdn.com/dms/image/v2/D4E03AQEfwXTMLTbUtA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1702997699915?e=1732147200&v=beta&t=7TyR2a4wUY61EHvuK6430jKUlfpq1AKILnL_3drBfhU',
+        name: 'Zeeshan',
+        courseName: 'Graphic Designing',
+        rollNo: 'GD - 908765'
+    },
+    {
+        imageURL: 'https://media.licdn.com/dms/image/v2/D4D03AQFuMdY5-V_lOw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1723990931643?e=1732147200&v=beta&t=EvYgqarwnoqpL95-g0J8DW6r8sLWC5rHweG-X45nmpA',
+        name: 'Mustafa',
+        courseName: '3D Animation',
+        rollNo: '3DA - 173678'
+    },
+]
+
+export default cardData;
--- app/page.js
+++ app/page.js
@@ -1,101 +1,44 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from 'react'
+import cardData from './cardData'
 
-export default function Home() {
+const page = () => {
+  const [index, setindex] = useState(0);
+
+  const handlePrev = () => {
+    if (index > 0) {
+      setindex(index - 1);
+    }
+  };
+  const handleNext = () => {
+    if (index < cardData.length - 1) {
+      setindex(index + 1);
+    }
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+    <div className='flex flex-col justify-center items-center gap-5 bg-gray-200 h-[100vh]'>
+      <div className='bg-white w-[20%] h-[55vh] shadow-lg shadow-gray-500 border border-gray-300 flex flex-col justify-between items-center'>
+        <div className='bg-blue-400 h-[3vh] w-full'></div>
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <img src='https://student.saylaniwelfare.com/assets/logo-OpazD70S.png' className='w-24' />
+          <img src={cardData[index].imageURL} className='w-28 h-28 border-2 border-green-400' />
+          <h1 className='text-xl uppercase'>{cardData[index].name}</h1>
+          <h1>{cardData[index].courseName}</h1>
+          <h1>{cardData[index].rollNo}</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className='bg-green-400 h-[3vh] w-full'></div>
+      </div>
+      <div className='flex items-center justify-between w-[20%]'>
+        <button className='py-2 px-6 bg-blue-500 text-white rounded-[19px] hover:bg-blue-600 duration-300' onClick={handlePrev}>PREV</button>
+        <button className='py-2 px-6 bg-blue-500 text-white rounded-[19px] hover:bg-blue-600 duration-300' onClick={handleNext}>NEXT</button>
+      </div>
+      <div>
+        <button className='py-2 px-6 bg-blue-500 text-white rounded-2xl hover:bg-blue-600 duration-300'>DOWNLOAD CARD</button>
+      </div>
     </div>
-  );
+    </>
+  )
 }
+
+export default page;
